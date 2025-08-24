77e2460ad76a9719c92352963fc5f62b7c604d98
alequerenghi <querenghialessandro@icloud.com>
2025-08-24 20:41:22 +0200

    completed local search, updated other files

--- bpp.py
+++ bpp.py
@@ -8,43 +8,45 @@ class BinPackingProblem:
 
     def __init__(self, energy_requirement: np.ndarray, battery_limit: float, J: int = 0) -> None:
         self.J = energy_requirement.shape[0] if J == 0 else J  # Number of jobs
+        self.R = self.J
         self.e = energy_requirement  # energy requirements
         self.b = battery_limit  # battery limit
-        self.m = None
 
     def solve(self) -> 'BinPackingProblem':
         m = gp.Model("bpp")
 
         # Binary variables!!!!
         # charging operations
-        self.gamma = m.addMVar(shape=self.J, vtype=GRB.BINARY)
-        self.chi = m.addMVar(shape=(self.J, self.J),
-                             vtype=GRB.BINARY)  # transfer jobs
+        gamma = m.addMVar(shape=self.J, vtype=GRB.BINARY)
+        chi = m.addMVar(shape=(self.R, self.J),
+                        vtype=GRB.BINARY)  # transfer jobs
 
         # Objective
-        m.setObjective(self.gamma.sum(), GRB.MINIMIZE)
+        # uguale a m.setObjective(gamma.sum(), GRB.MINIMIZE)
+        m.setObjective(gamma @ np.ones(self.J), GRB.MINIMIZE)
 
         # Constraints!!!
         # Assign each job to a charging operation
-        m.addConstr(self.chi.sum(axis=0) == 1)
+        # uguale a m.addConstr(chi.sum(axis=0) == 1)
+        m.addConstr(np.ones(self.R) @ chi == 1)
         # If charge used, then sum of jobs is lower than capacity
-        m.addConstr((self.chi @ self.e) <= (self.b*self.gamma))
-        m.addConstrs(self.gamma[r] <= self.gamma[r-1]
+        m.addConstr((chi @ self.e) <= (self.b*gamma))
+        m.addConstrs(gamma[r] <= gamma[r-1]
                      for r in range(1, self.J))  # Symmetry breaking
 
         m.setParam('OutputFlag', 0)
         m.optimize()
-        self.m = m
+
+        self.gamma = gamma
+        self.chi = chi
+        self.zeta = m.ObjVal
 
         return self
 
     def get_lower_bound(self, M: int, t: float, d: np.ndarray) -> float:
-        if self.m is not None:
-            first = ceil((max(0, self.m.ObjVal-M)*t+d.sum())/M)
-            second = ceil(max(0, self.m.ObjVal-M)/M)*t
-            return max(first, second)
-        else:
-            raise RuntimeError("Forgot to solve first!!!")
+        first = ceil((max(0, self.zeta-M)*t+d.sum())/M)
+        second = ceil(max(0, self.zeta-M)/M)*t
+        return max(first, second)
 
 
 # e = np.array([50, 3, 48, 53, 53, 4, 3, 41, 23, 20, 52, 49])
