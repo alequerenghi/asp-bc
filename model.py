ca95ca30ac95e7a6167e0c7fbc77d0afc22205b3
alequerenghi <querenghialessandro@icloud.com>
2025-09-08 13:56:06 +0200

    implement output function

--- model.py
+++ model.py
@@ -4,6 +4,8 @@ from numpy.typing import NDArray
 import gurobipy as gb
 from gurobipy import GRB, quicksum
 from parser import parse_file
+from csv import writer
+
 
 class ASPBC:
     def __init__(self, job_number: int, agv_number: int, job_durations: NDArray[np.int64],
@@ -18,64 +20,75 @@ class ASPBC:
         self.e = energy_requirements
         self.R = self.J if charging_operations_number == 0 else charging_operations_number
 
-    def solve(self, env = None) -> "ASPBC":
+    def solve(self, env=None) -> "ASPBC":
         aspbc = gb.Model("ASP-BC", env=env)
-	
-        x = aspbc.addVars([(j,m) for j in range(self.J) for m in range(self.M)], vtype=GRB.BINARY) 
-        q = aspbc.addVars([(r,m) for r in range(self.R) for m in range(self.M)], vtype=GRB.BINARY) 
-        y = aspbc.addVars([(j,r,m) for j in range(self.J) for r in range(self.R) for m in range(self.M)], vtype=GRB.BINARY)
-        
+
+        x = aspbc.addVars([(j, m) for j in range(self.J)
+                          for m in range(self.M)], vtype=GRB.BINARY)
+        q = aspbc.addVars([(r, m) for r in range(self.R)
+                          for m in range(self.M)], vtype=GRB.BINARY)
+        y = aspbc.addVars([(j, r, m) for j in range(self.J) for r in range(
+            self.R) for m in range(self.M)], vtype=GRB.BINARY)
+
         cmax = aspbc.addVar(vtype=GRB.CONTINUOUS, lb=0.0)
 
         aspbc.setObjective(cmax, GRB.MINIMIZE)
 
-        aspbc.addConstrs(cmax >= quicksum(self.d[j] * x[j,m] for j in range(self.J)) + quicksum(self.t * q[r,m] for r in range(1, self.R))
-                          for m in range(self.M)
-                        )
-        aspbc.addConstrs(quicksum(x[j,m] for m in range(self.M)) == 1 
-                          for j in range(self.J)
-                        )
-        aspbc.addConstrs(quicksum(y[j,r,m] for r in range(self.R)) == x[j, m] 
-                          for j in range(self.J) 
-                          for m in range(self.M)
-                        )
-        aspbc.addConstrs(2 * y[j,r,m] <= x[j,m] + q[r,m] 
-                          for j in range(self.J) 
-                          for r in range(self.R) 
-                          for m in range(self.M)
-                        )
-        aspbc.addConstrs(quicksum(self.e[j] * y[j,r,m] for j in range(self.J)) <= self.b 
-                          for r in range(self.R) 
-                          for m in range(self.M)
-                        )
-        aspbc.addConstrs(q[r,m] <= q[r-1,m] 
-                          for r in range(1, self.R)
-                          for m in range(self.M)
-                        )
-        aspbc.addConstrs(q[1,m] == 1 
-                          for m in range(self.M)
-                        )
+        aspbc.addConstrs(cmax >= quicksum(self.d[j] * x[j, m] for j in range(self.J)) + quicksum(self.t * q[r, m] for r in range(1, self.R))
+                         for m in range(self.M)
+                         )
+        aspbc.addConstrs(quicksum(x[j, m] for m in range(self.M)) == 1
+                         for j in range(self.J)
+                         )
+        aspbc.addConstrs(quicksum(y[j, r, m] for r in range(self.R)) == x[j, m]
+                         for j in range(self.J)
+                         for m in range(self.M)
+                         )
+        aspbc.addConstrs(2 * y[j, r, m] <= x[j, m] + q[r, m]
+                         for j in range(self.J)
+                         for r in range(self.R)
+                         for m in range(self.M)
+                         )
+        aspbc.addConstrs(quicksum(self.e[j] * y[j, r, m] for j in range(self.J)) <= self.b
+                         for r in range(self.R)
+                         for m in range(self.M)
+                         )
+        aspbc.addConstrs(q[r, m] <= q[r-1, m]
+                         for r in range(1, self.R)
+                         for m in range(self.M)
+                         )
+        aspbc.addConstrs(q[0, m] == 1
+                         for m in range(self.M)
+                         )
         aspbc.optimize()
 
-        self.x = x
-        self.y = y
-        self.q = q
+        self.aspbc = aspbc
 
         return self
-    
+
     @classmethod
     def create_from_file(cls, file_name: str) -> "ASPBC":
-        job_number, agv_number, job_durations, battery_capacity, charge_duration, energy_requirements = parse_file(file_name)
-        return cls(job_number, agv_number, job_durations, battery_capacity, charge_duration, energy_requirements)
+        return cls(*parse_file(
+            file_name))
+
 
 def read_and_solve(folder) -> None:
-    with gb.Env() as env:
-        env.setParam("TimeLimit", 1800)
-        for fname in sorted(os.listdir(folder)):
-            if fname.endswith(".txt"): 
-                path = os.path.join(folder, fname)
-                model = ASPBC.create_from_file(path)
-                model.solve(env)
+    with open("output.csv", "wt") as output_file:
+        csv_writer = writer(output_file)
+        csv_writer.writerow(["Name", "Machines", "Jobs",
+                            "Lower bound", "Upper bound", "Gap", "Time"])
+        with gb.Env() as env:
+            env.setParam("OutputFlag", 0)
+            env.setParam("ConcurrentMIP", 3)
+            env.setParam("TimeLimit", 1800)
+            for fname in os.listdir(folder):
+                if fname.endswith(".txt"):
+                    path = os.path.join(folder, fname)
+                    model = ASPBC.create_from_file(path)
+                    model.solve(env)
+                    csv_writer.writerow(
+                        [fname, model.M, model.J, model.aspbc.ObjVal, model.aspbc.ObjBound, model.aspbc.MIPGap, model.aspbc.Runtime])
+
 
 if __name__ == "__main__":
     read_and_solve("dataset/ASP-BC Instances")
--- parser.py
+++ parser.py
@@ -3,6 +3,7 @@ import re
 import numpy as np
 from numpy.typing import NDArray
 
+
 def parse_file(filepath) -> tuple[int, int, NDArray[np.int64], float, float, NDArray[np.float64]]:
     with open(filepath, "r") as f:
         instance = f.read()
@@ -10,21 +11,29 @@ def parse_file(filepath) -> tuple[int, int, NDArray[np.int64], float, float, NDA
     agv_number = int(re.search(r"N_MACHINES:(\d+)", instance).group(1))
     job_number = int(re.search(r"N_JOBS:(\d+)", instance).group(1))
     charge_duration = int(re.search(r"CHARGING_TIME:(\d+)", instance).group(1))
-    battery_capacity = int(re.search(r"INITIAL_CHARGE:(\d+)", instance).group(1))
+    battery_capacity = int(
+        re.search(r"INITIAL_CHARGE:(\d+)", instance).group(1))
 
     D_block = re.search(r"D:\[(.*?)\]", instance, re.S).group(1)
-    D_lines = [line.strip() for line in D_block.strip().splitlines() if line.strip()]
+    D_lines = [line.strip()
+               for line in D_block.strip().splitlines() if line.strip()]
     D = [[int(x) for x in re.split(r"\s+", line)] for line in D_lines]
 
     W_block = re.search(r"w:\[(.*?)\]", instance, re.S).group(1)
-    W_lines = [line.strip() for line in W_block.strip().splitlines() if line.strip()]
+    W_lines = [line.strip()
+               for line in W_block.strip().splitlines() if line.strip()]
     W = [[float(x) for x in re.split(r"\s+", line)] for line in W_lines]
 
     job_durations = np.array(D)[:, 0]
     energy_requirements = np.array(W)[:, 0]
 
-    assert len(job_durations) == job_number, f"Mismatch: expected {job_number} jobs, got {len(job_durations)}"
-    assert len(energy_requirements) == job_number, f"Mismatch: expected {job_number} jobs, got {len(energy_requirements)}"
+    assert len(
+        job_durations) == job_number, f"Mismatch: expected {job_number} jobs, got {len(job_durations)}"
+    assert len(
+        energy_requirements) == job_number, f"Mismatch: expected {job_number} jobs, got {len(energy_requirements)}"
 
     return (job_number, agv_number, job_durations, battery_capacity, charge_duration, energy_requirements)
 
+
+if __name__ == "__main__":
+    parse_file("dataset/ASP-BC Instances/Ins_V10_J200_T30_R60_B10_W2_S357_N7.txt")
