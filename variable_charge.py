405e153a4d3df4825c30bf9d1489758e7f60c3a5
alequerenghi <querenghialessandro@icloud.com>
2025-09-10 11:41:27 +0200

    implemented variable charge

--- variable_charge.py
+++ variable_charge.py
@@ -1,52 +1,109 @@
 
 
+import os
 from gurobipy import GRB
 import scipy.sparse as sp
 import numpy as np
 import gurobipy as gb
+from gurobipy import quicksum
+from numpy.typing import NDArray
+from parser import parse_file
+import csv
 
 
-def solve(R: int, J: int, M: int, computation_times: np.ndarray, e: np.ndarray, tau: float):
-    model = gb.Model("ASP-BC-v")
+class ASPBC:
+    def __init__(self, agv_number: int, job_durations: NDArray[np.int64],
+                 battery_capacity: float, charge_duration: float, energy_requirements: NDArray[np.float64],
+                 charging_operations_number=0
+                 ) -> None:
+        self.M = agv_number
+        self.d = job_durations
+        self.b = battery_capacity
+        self.t = charge_duration
+        self.e = energy_requirements
+        self.R = energy_requirements.shape[0] if charging_operations_number == 0 else charging_operations_number
 
-    x = model.addMVar((J, M), vtype=GRB.BINARY)
-    y = model.addMVar((R, J, M), vtype=GRB.BINARY)
-    q = model.addMVar((R, M), vtype=GRB.BINARY)
-    cmax = model.addVar()
+    def solve(self, tau: float, env=None):
+        J = self.e.shape[0]
 
-    # m.addConstr(cmax >= computation_times @ x + tau * ((energy_consumptions @ y)[-1:] * q[1:]).sum(axis=0))
+        model = gb.Model("ASP-BC-v", env=env)
+        x = model.addVars([(j, m) for j in range(J)
+                          for m in range(self.M)], vtype=GRB.BINARY, name="x")
+        q = model.addVars([(r, m) for r in range(self.R)
+                          for m in range(self.M)], vtype=GRB.BINARY, name="q")
+        y = model.addVars([(j, r, m) for j in range(J) for r in range(
+            self.R) for m in range(self.M)], vtype=GRB.BINARY, name="y")
+        cmax = model.addVar()
 
-    # r = 0..R-2, j = 0..J-1, m = 0..M-1
-    w = model.addVars(R-1, J, M, vtype=GRB.BINARY)
+        # m.addConstr(cmax >= computation_times @ x + tau * ((energy_consumptions @ y)[-1:] * q[1:]).sum(axis=0))
 
-    model.addConstrs(
-        (w[r, j, m] <= y[r, j, m] for r in range(R-1)
-         for j in range(J) for m in range(M))
-    )
+        # r = 0..R-2, j = 0..J-1, m = 0..M-1
+        w = model.addVars(self.R-1, J, self.M, vtype=GRB.BINARY, name="w")
 
-    model.addConstrs(
-        (w[r, j, m] <= q[r+1, m] for r in range(R-1)
-         for j in range(J) for m in range(M))
-    )
+        model.setObjective(cmax, GRB.MINIMIZE)
 
-    model.addConstrs(
-        (w[r, j, m] >= y[r, j, m] + q[r+1, m] - 1
-         for r in range(R-1) for j in range(J) for m in range(M))
-    )
+        # w <= y[:R-1]
+        model.addConstrs(
+            (w[r, j, m] <= y[r, j, m] for r in range(self.R-1)
+             for j in range(J) for m in range(self.M))
+        )
 
-    energy_term = model.addVars(M, lb=0)
+        # w <= q[1:, None, :]
+        model.addConstrs(
+            (w[r, j, m] <= q[r+1, m] for r in range(self.R-1)
+             for j in range(J) for m in range(self.M))
+        )
 
-    model.addConstrs(
-        (energy_term[m] == sum(e[j] * w[r, j, m]
-                               for r in range(R-1) for j in range(J)) for m in range(M))
-    )
+        # w >= y[:R-1] + q[1:, None, :] -1
+        model.addConstrs(
+            (w[r, j, m] >= y[r, j, m] + q[r+1, m] - 1
+             for r in range(self.R-1) for j in range(J) for m in range(self.M))
+        )
 
-    jobs_term = model.addVars(M, lb=0)
+        # cmax >= d @ x + tau*(e @ w).sum(axis=0)
+        model.addConstrs(cmax >= quicksum(self.d[j] * x[j, m] for j in range(J)) + tau * quicksum(
+            self.e[j] * w[r, j, m]for r in range(self.R-1) for j in range(J)) for m in range(self.M))
 
-    model.addConstrs(
-        (jobs_term[m] == sum(computation_times[j] * x[j, m] for j in range(J))
-         for m in range(M))
-    )
+        model.addConstrs(quicksum(x[j, m] for m in range(self.M)) == 1
+                         for j in range(J)
+                         )
+        model.addConstrs(quicksum(y[r, j, m] for r in range(self.R)) == x[j, m]
+                         for j in range(J)
+                         for m in range(self.M)
+                         )
+        model.addConstrs(2 * y[r, j, m] <= x[j, m] + q[r, m]
+                         for j in range(J)
+                         for r in range(self.R)
+                         for m in range(self.M)
+                         )
+        model.addConstrs(quicksum(self.e[j] * y[r, j, m] for j in range(J)) <= self.b
+                         for r in range(self.R)
+                         for m in range(self.M)
+                         )
+        model.addConstrs(q[r, m] <= q[r-1, m]
+                         for r in range(1, self.R)
+                         for m in range(self.M)
+                         )
+        model.addConstrs(q[0, m] == 1
+                         for m in range(self.M)
+                         )
+        model.optimize()
 
-    model.addConstrs((cmax >= jobs_term[m] + tau *
-                      energy_term[m] for m in range(M)))
+        self.aspbc = model
+
+        return self
+
+    @classmethod
+    def create_from_file(cls, file_name: str) -> "ASPBC":
+        return cls(*parse_file(
+            file_name))
+
+
+if __name__ == "__main__":
+    m = ASPBC.create_from_file(
+        "dataset/ASP-BC Instances/Ins_V2_J50_T10_R60_B10_W1_S90_N0.txt")
+    m.solve(1.0)
+    for v in m.aspbc.getVars():
+        if "x" in v.VarName:
+            print(f"{v.VarName} = {v.X}")
+    print(f"Objective: {m.aspbc.ObjVal}")
