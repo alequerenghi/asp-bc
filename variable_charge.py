e08cee818fc383fe647732a2bf2f2137dc4b20c6
alequerenghi <querenghialessandro@icloud.com>
2025-09-10 12:31:22 +0200

    vectorized variable charge

--- variable_charge.py
+++ variable_charge.py
@@ -27,66 +27,24 @@ class ASPBC:
         J = self.e.shape[0]
 
         model = gb.Model("ASP-BC-v", env=env)
-        x = model.addVars([(j, m) for j in range(J)
-                          for m in range(self.M)], vtype=GRB.BINARY, name="x")
-        q = model.addVars([(r, m) for r in range(self.R)
-                          for m in range(self.M)], vtype=GRB.BINARY, name="q")
-        y = model.addVars([(j, r, m) for j in range(J) for r in range(
-            self.R) for m in range(self.M)], vtype=GRB.BINARY, name="y")
+        x = model.addMVar((J, self.M), vtype=GRB.BINARY)
+        q = model.addMVar((self.R, self.M), vtype=GRB.BINARY)
+        y = model.addMVar((J, self.R, self.M), vtype=GRB.BINARY)
+        w = model.addMVar((J, self.R-1, self.M), vtype=GRB.BINARY)
         cmax = model.addVar()
-
-        # m.addConstr(cmax >= computation_times @ x + tau * ((energy_consumptions @ y)[-1:] * q[1:]).sum(axis=0))
-
-        # r = 0..R-2, j = 0..J-1, m = 0..M-1
-        w = model.addVars(self.R-1, J, self.M, vtype=GRB.BINARY, name="w")
 
         model.setObjective(cmax, GRB.MINIMIZE)
 
-        # w <= y[:R-1]
-        model.addConstrs(
-            (w[r, j, m] <= y[r, j, m] for r in range(self.R-1)
-             for j in range(J) for m in range(self.M))
-        )
-
-        # w <= q[1:, None, :]
-        model.addConstrs(
-            (w[r, j, m] <= q[r+1, m] for r in range(self.R-1)
-             for j in range(J) for m in range(self.M))
-        )
-
-        # w >= y[:R-1] + q[1:, None, :] -1
-        model.addConstrs(
-            (w[r, j, m] >= y[r, j, m] + q[r+1, m] - 1
-             for r in range(self.R-1) for j in range(J) for m in range(self.M))
-        )
-
-        # cmax >= d @ x + tau*(e @ w).sum(axis=0)
-        model.addConstrs(cmax >= quicksum(self.d[j] * x[j, m] for j in range(J)) + tau * quicksum(
-            self.e[j] * w[r, j, m]for r in range(self.R-1) for j in range(J)) for m in range(self.M))
-
-        model.addConstrs(quicksum(x[j, m] for m in range(self.M)) == 1
-                         for j in range(J)
-                         )
-        model.addConstrs(quicksum(y[r, j, m] for r in range(self.R)) == x[j, m]
-                         for j in range(J)
-                         for m in range(self.M)
-                         )
-        model.addConstrs(2 * y[r, j, m] <= x[j, m] + q[r, m]
-                         for j in range(J)
-                         for r in range(self.R)
-                         for m in range(self.M)
-                         )
-        model.addConstrs(quicksum(self.e[j] * y[r, j, m] for j in range(J)) <= self.b
-                         for r in range(self.R)
-                         for m in range(self.M)
-                         )
-        model.addConstrs(q[r, m] <= q[r-1, m]
-                         for r in range(1, self.R)
-                         for m in range(self.M)
-                         )
-        model.addConstrs(q[0, m] == 1
-                         for m in range(self.M)
-                         )
+        model.addConstr(w <= y[:, :-1])
+        model.addConstr(w <= q[None, 1:, :])
+        model.addConstr(w >= y[:, :-1] + q[None, 1:, :]-1)
+        model.addConstr(cmax >= self.d @ x + tau * (self.e @ w.sum(axis=1)))
+        model.addConstr(x @ np.ones(self.M) == 1)
+        model.addConstr(y.sum(axis=1) == x)
+        model.addConstr(2 * y <= x[:, None, :] + q[None, :, :])
+        model.addConstr((self.e[None, :, None] * y).sum(axis=0) <= self.b)
+        model.addConstr(q[1:] <= q[:-1])
+        model.addConstr(q[0] == 1)
         model.optimize()
 
         self.aspbc = model
@@ -100,10 +58,8 @@ class ASPBC:
 
 
 if __name__ == "__main__":
-    m = ASPBC.create_from_file(
-        "dataset/ASP-BC Instances/Ins_V2_J50_T10_R60_B10_W1_S90_N0.txt")
-    m.solve(1.0)
-    for v in m.aspbc.getVars():
-        if "x" in v.VarName:
-            print(f"{v.VarName} = {v.X}")
-    print(f"Objective: {m.aspbc.ObjVal}")
+    with gb.Env() as env:
+        env.setParam("ConcurrentMIP", 3)
+        m = ASPBC.create_from_file(
+            "dataset/ASP-BC Instances/Ins_V2_J50_T10_R60_B10_W1_S90_N0.txt")
+        m.solve(1.0, env)
